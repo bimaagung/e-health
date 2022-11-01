8b7bfd8e76cc26ece3190f0cb28806ceef39ba57
irvan taufik <irvantaufik28@gmail.com>
2022-11-01 15:09:56 +0700

    otp model

---
+++ src/database/migrations/20221101080923-create-otp.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Otps', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      otp_type: {
+        type: Sequelize.STRING
+      },
+      otp_code: {
+        type: Sequelize.STRING
+      },
+      expired_at: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Otps');
+  }
+};
