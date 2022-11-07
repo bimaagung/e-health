c93555126d3b6c1eb33fee17222f6fab568bcaa7
irvan taufik <irvantaufik28@gmail.com>
2022-11-07 10:22:47 +0700

    merger

--- src/app.js
+++ src/app.js
@@ -12,38 +12,22 @@ const app = express();
 const typeOtp = require('./internal/constant/typeOtp');
 
 // User Case
-const AuthUseCase = require('./usecase/auth');
 const CategoryUseCase = require('./usecase/category');
 const OTPUseCase = require('./usecase/otp');
 const AuthseCase = require('./usecase/auth');
 
 // Repository
-const AuthRepository = require('./repository/auth');
-const UserRepository = require('./repository/user');
-const OtpRepository = require('./repository/otp');
-const EmailRepository = require('./repository/email');
 const CategoryRepository = require('./repository/category');
 const OTPRepository = require('./repository/otp');
 const EmailRepository = require('./repository/email');
 const UserRepository = require('./repository/user');
 
 // Router
-const authRouter = require('./routes/auth');
-const otpRouter = require('./routes/otp');
 const adminRouter = require('./routes/admin');
 const categoryRouter = require('./routes/category');
 const otpRouter = require('./routes/otp');
 const authRouter = require('./routes/auth');
 
-const authUC = new AuthUseCase(
-  new AuthRepository(),
-  new UserRepository(),
-  new EmailRepository(),
-  new OtpRepository(),
-  bcrypt,
-  func,
-  _,
-);
 const categoryUC = new CategoryUseCase(new CategoryRepository(), mediaHandler);
 const otpUC = new OTPUseCase(new OTPRepository(), new EmailRepository(), typeOtp);
 const userUC = new AuthseCase(new UserRepository(), bcrypt, tokenManager);
@@ -53,7 +37,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  req.authUC = authUC;
   req.categoryUC = categoryUC;
   req.otpUC = otpUC;
   req.userUC = userUC;
@@ -64,8 +47,6 @@ app.get('/', (req, res) => {
   res.send('Welcome E-Health');
 });
 
-app.use('/api', authRouter);
-app.use('/api', otpRouter);
 app.use('/api/admin', adminRouter);
 app.use('/api/category', categoryRouter);
 app.use('/api/otp', otpRouter);
