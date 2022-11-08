0dd9f69da02beb41db1839e19862ca2acc27e306
irvan taufik <irvantaufik28@gmail.com>
2022-11-08 22:38:35 +0700

    eslint

--- src/usecase/docterValidation.js
+++ src/usecase/docterValidation.js
@@ -38,22 +38,20 @@ class docterValidationUseCase {
     validation.status = this._validationStatus.PENDING;
     const validationExist = await this._docterValidation.getDocterValdationByUserId(validation.docterId);
     if (validationExist === null) {
-      const createValidation = await this._docterValidation.addDocterValidation(
-        validation,
-        );
-        result.isSuccess = true;
-        result.statusCode = 201;
-        result.data = createValidation;
-        
-        return result;
-      }
-      // check validation PENDING AND COMPLETED
-    for (let i = 0; i < validationExist.length; i++) {
+      const createValidation = await this._docterValidation.addDocterValidation(validation);
+      result.isSuccess = true;
+      result.statusCode = 201;
+      result.data = createValidation;
+
+      return result;
+    }
+    // check validation PENDING AND COMPLETED
+    for (let i = 0; i < validationExist.length; i ++) {
       if (validationExist[i].status === this._validationStatus.PENDING) {
         result.statusCode = 400;
         result.reason = 'you have sent the doc, please check your email regularly for update';
         return result;
-      } 
+      }
       if (validationExist[i].status === this._validationStatus.COMPLETED) {
         result.statusCode = 400;
         result.reason = 'you have been verified as a doctor';
@@ -61,9 +59,7 @@ class docterValidationUseCase {
       }
     }
     // create new Validation If status validation REJECT
-    const createValidation = await this._docterValidation.addDocterValidation(
-      validation,
-    );
+    const createValidation = await this._docterValidation.addDocterValidation(validation);
     result.isSuccess = true;
     result.statusCode = 201;
     result.data = createValidation;
