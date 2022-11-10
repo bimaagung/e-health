9e26d87b839287f7d07b523da59c3d33c728bd5c
irvan taufik <irvantaufik28@gmail.com>
2022-11-10 13:01:41 +0700

    get docter byId

--- src/routes/doctor.js
+++ src/routes/doctor.js
@@ -6,5 +6,6 @@ const router = express.Router();
 const { authorized } = require('../middleware/authorization');
 
 router.get('/list', doctorController.getAllDoctor);
+router.get('/detail/:id', doctorController.getDoctorByUserId);
 
 module.exports = router;
