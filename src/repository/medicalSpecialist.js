a6e42861941910b9adca4b0bfaee0015f5fd92da
irvan taufik <irvantaufik28@gmail.com>
2022-11-10 15:09:13 +0700

    medicalSpecialist

---
+++ src/repository/medicalSpecialist.js
@@ -0,0 +1,48 @@
+const { MedicalSpecialist } = require('../models');
+
+class MedicalSpecialistRepository {
+  constructor() {
+    this._medicalSpecialistModel = MedicalSpecialist;
+  }
+
+  async addMedicalSpecialist(specialist) {
+    const result = await this._medicalSpecialistModel.create({ name: specialist });
+    return result;
+  }
+
+  async getListMedicalSpecialist() {
+    const result = await this._medicalSpecialistModel.findAll();
+    return result;
+  }
+
+  async getMedicalSpecialistById(id) {
+    const result = await this._medicalSpecialistModel.findOne(
+      {
+        where: { id },
+      },
+    );
+
+    return result;
+  }
+
+  async updateMedicalSpecialist(id, specialist) {
+    const result = await this._medicalSpecialistModel.findOne(
+      specialist,
+      {
+        where: { id },
+      },
+    );
+    return result;
+  }
+
+  async deleteMedicalSpecialist(id) {
+    const result = await this._medicalSpecialistModel.destroy(
+      {
+        where: { id },
+      },
+    );
+    return result;
+  }
+}
+
+module.exports = MedicalSpecialistRepository;
