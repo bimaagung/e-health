ebf0e76cfc2708b22bdb3c7b88dc3f57d83f2f52
bimaagung <bimaagung.labs@gmail.com>
2022-11-15 09:32:50 +0700

    add test update product

--- src/test/usecase/usecase.product.spec.js
+++ src/test/usecase/usecase.product.spec.js
@@ -9,6 +9,26 @@ let productUC = null;
 
 
 describe('product test', () => { 
+
+    let product = {
+        name: 'Paracetamol 10MG',
+        categoryId:1,
+        isStrip: true,
+        price: 300000,
+        description: 'obat penurun panas',
+        indication: 'obat untuk menurunkan gejala panas',
+        composition: 'Zaproles 5mg',
+        dose: 'Sehari 3X',
+        use: 'Diminum sesudah makan',
+        sideEffect: 'alergi seperti pusing',
+        segmentation: 'MERAH',
+        packaging: 'isi 10 strip',
+        manufacture: 'PT. Husada Karya Daria',
+        stock: 20,
+        urlImage: null,
+        expiredAt: '2023-09-07',
+    }
+
     beforeEach(() => {
         mockProductReturn = {
             getListProduct: jest.fn().mockReturnValue([mockProduct.product]),
@@ -32,24 +52,7 @@ describe('product test', () => {
     });
 
     describe('addProduct test', () => { 
-         let product = {
-                name: 'Paracetamol 10MG',
-                categoryId:1,
-                isStrip: true,
-                price: 300000,
-                description: 'obat penurun panas',
-                indication: 'obat untuk menurunkan gejala panas',
-                composition: 'Zaproles 5mg',
-                dose: 'Sehari 3X',
-                use: 'Diminum sesudah makan',
-                sideEffect: 'alergi seperti pusing',
-                segmentation: 'MERAH',
-                packaging: 'isi 10 strip',
-                manufacture: 'PT. Husada Karya Daria',
-                stock: 20,
-                urlImage: null,
-                expiredAt: '2023-09-07',
-            }
+         
         test("should isSuccess = true, statusCode = 200, and data is true", async () => {
             mockProductReturn.getProductByName = jest.fn().mockReturnValue(null);
             productUC = new ProductUseCase(mockProductReturn, mockCategoryReturn, mediaHandler);
@@ -106,4 +109,54 @@ describe('product test', () => {
             expect(res.data.categoryId).toEqual(1);
         });
      });
+
+     describe('updateProduct test', () => { 
+        beforeEach(() => {
+            
+        });
+
+        test("should isSuccess = true,statusCode = 200", async () => {
+            let res = await productUC.updateProduct(id, product, { path:'C:/Image.jpg' });
+
+            expect(mockProductReturn.getProductById).toHaveBeenCalled();
+            expect(mockCategoryReturn.getCategoryById).toHaveBeenCalled();
+            expect(mediaHandler.cloudinaryUpload).toHaveBeenCalled();
+            expect(mockProductReturn.updateProduct).toHaveBeenCalled();
+
+            expect(res.isSuccess).toBeTruthy();
+            expect(res.statusCode).toEqual(200);
+
+        });
+        test("should isSuccess = false, statusCode = 404, and reason = 'product not found'", async () => {
+            mockProductReturn.getProductById = jest.fn().mockReturnValue(null);
+            productUC = new ProductUseCase(mockProductReturn, mockCategoryReturn, mediaHandler);
+
+            let res = await productUC.updateProduct(1, product, { path:'C:/Image.jpg' });
+
+            expect(res.isSuccess).toBeFalsy();
+            expect(res.statusCode).toEqual(404);
+            expect(res.reason).toEqual('product not found');
+
+        });
+
+        test("should isSuccess = false, statusCode = 404, and reason = 'category not found'", async () => {
+            mockCategoryReturn.getCategoryById = jest.fn().mockReturnValue(null);
+            productUC = new ProductUseCase(mockProductReturn, mockCategoryReturn, mediaHandler);
+            
+            let res = await productUC.updateProduct(1, product, { path:'C:/Image.jpg' });
+
+            expect(res.isSuccess).toBeFalsy();
+            expect(res.statusCode).toEqual(404);
+            expect(res.reason).toEqual('category not found');
+        });
+
+        test("when image empty should isSuccess = true,statusCode = 200", async () => {
+            productUC = new ProductUseCase(mockProductReturn, mockCategoryReturn, mediaHandler);
+            
+            let res = await productUC.updateProduct(1, product, { path:'' });
+
+            expect(res.isSuccess).toBeTruthy();
+            expect(res.statusCode).toEqual(200);
+        });
+      })
  });
